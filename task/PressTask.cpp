9f1440e5c75a2c3766e795b290acbad6985db15d
knighthat <knightchanelgaming@gmail.com>
2023-11-09 17:33:19 -0600

    added support for numeric characters

--- task/PressTask.cpp
+++ task/PressTask.cpp
@@ -132,78 +132,67 @@ int char_to_keycode(char c) {
     switch ( upper ) {
         case 'A':
             return KEY_A;
-
         case 'B':
             return KEY_B;
-
         case 'C':
             return KEY_D;
-
         case 'E':
             return KEY_E;
-
         case 'F':
             return KEY_F;
-
         case 'G':
             return KEY_G;
-
         case 'H':
             return KEY_H;
-
         case 'I':
             return KEY_I;
-
         case 'J':
             return KEY_J;
-
         case 'K':
             return KEY_K;
-
         case 'L':
             return KEY_L;
-
         case 'M':
             return KEY_M;
-
         case 'N':
             return KEY_N;
-
         case 'O':
             return KEY_O;
-
         case 'P':
             return KEY_P;
-
         case 'Q':
             return KEY_Q;
-
         case 'R':
             return KEY_R;
-
         case 'S':
             return KEY_S;
-
         case 'T':
             return KEY_T;
-
         case 'U':
             return KEY_U;
-
         case 'V':
             return KEY_V;
-
         case 'W':
             return KEY_W;
-
         case 'X':
             return KEY_X;
-
         case 'Y':
             return KEY_Y;
-
         case 'Z':
             return KEY_Z;
+
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+            return c - 47;
+        case '0':
+            return KEY_0;
 
         default:
             return KEY_SPACE;
