199a108c9164d6f001c2e1822b12c3c99043a4ff
knighthat <knightchanelgaming@gmail.com>
2023-11-24 10:39:22 -0600

    added loop & script performs tasks after lexing

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <memory>
 
 #include "Utils.h"
 #include "token/Token.h"
@@ -24,9 +25,52 @@
 using namespace std;
 
 
+class Script {
+
+private:
+    void run0() {
+        for (const auto& task: tasks)
+            task->execute();
+    }
+
+public:
+    int repeat = 1;
+    vector<unique_ptr<Task>> tasks;
+
+    [[noreturn]] void run() {
+      if (repeat == 0) {
+          while (true) {
+              run0();
+          }
+      }
+      else if (repeat > 0){
+          for (int i = 0 ; i < repeat ; i++){
+              run0();
+          }
+      }
+  }
+};
+
+void print_help(char *file_name) {
+    cout << "Usage: " << file_name << " <file> --[option]" << endl;
+    cout << "  - loop [times]: Repeat this script [times] times. '0' will repeat forever";
+}
+
 int main(int argc, char **argv) {
     if (argc < 2) {
         cout << "Missing file to read" << endl;
+        print_help(argv[0]);
+        return 1;
+    }
+
+    Script script;
+
+    if (argc == 4 || string(argv[2]) != "--loop") {
+
+        script.repeat = stoi(argv[3]);
+
+    } else {
+        print_help(argv[0]);
         return 1;
     }
 
@@ -53,34 +97,33 @@ int main(int argc, char **argv) {
         }
 
         string data = trim(parts[1]);
-        Task *task = nullptr;
+        unique_ptr<Task> task_ptr;
         switch (*keyword) {
 
             case Keyword::MOVE:
                 if (MoveMouseTask::validate(data))
-                    task = new MoveMouseTask(data);
-                break;
-            case Keyword::PRESS:
-                if (PressTask::validate(data))
-                    task = new PressTask(data);
-                break;
-            case Keyword::WRITE:
-                if (WriteTask::validate(data))
-                    task = new WriteTask(data);
-                break;
-            case Keyword::WAIT:
-                if (WaitTask::validate(data))
-                    task = new WaitTask(data);
+                    task_ptr = make_unique<MoveMouseTask>(data);
                 break;
             case Keyword::CLICK:
                 if (MouseClickTask::validate(data))
-                    task = new MouseClickTask(data);
+                    task_ptr = make_unique<MouseClickTask>(data);
+                break;
+            case Keyword::PRESS:
+                if (PressTask::validate(data))
+                    task_ptr = make_unique<PressTask>(data);
+                break;
+            case Keyword::WRITE:
+                if (WriteTask::validate(data))
+                    task_ptr = make_unique<WriteTask>(data);
+                break;
+            case Keyword::WAIT:
+                if (WaitTask::validate(data))
+                    task_ptr = make_unique<WaitTask>(data);
                 break;
         }
 
-        if (task == nullptr)
-            return 1;
-        else
-            task->execute();
+        script.tasks.push_back(std::move(task_ptr));
     }
+
+    script.run();
 }
